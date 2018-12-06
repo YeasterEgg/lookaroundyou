9bde4ac2224ed5cc99c8246728042662b8ca198b
Yeasteregg <l.d.mattiazzi@gmail.com>
2018-12-06 19:13:59 +0100

    first

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,29 +1,33 @@
 import { get } from 'lodash'
+import pMap from 'p-map'
 
-// const STATUS = {
-//   NOTHING: 0,
-//   SOMETHING: 1,
-//   ACCESSIBLE: 2,
-// }
-// const TIMEOUT = 20
+const STATUS = {
+  TIMEOUT: 0,
+  ERROR: 1,
+  LOADED: 2,
+}
+const TIMEOUT = 1000
 
-// const timeoutPromiseFactory = (val: any): Promise<any> => {
-//   return new Promise(resolve => setTimeout(() => resolve(val), TIMEOUT))
-// }
+const timeoutPromiseFactory = (val: any, img: HTMLImageElement): Promise<any> => {
+  return new Promise(resolve =>
+    setTimeout(() => {
+      img.src = ''
+      img = null
+      resolve(val)
+    }, TIMEOUT),
+  )
+}
 
-// const pinger = () => {
-//   const image = new Image()
-//   const testUrl = (url: string) => {
-//     const imagePromise = new Promise(resolve => {
-//       image.onload = () => resolve(STATUS.ACCESSIBLE)
-//       image.onerror = () => resolve(STATUS.SOMETHING)
-//       image.src = url
-//     })
-//     const timeoutPromise = timeoutPromiseFactory(STATUS.NOTHING)
-//     return Promise.race([imagePromise, timeoutPromise])
-//   }
-//   return testUrl
-// }
+const ping = (url: string): Promise<number> => {
+  let image = new Image()
+  const imagePromise = new Promise(resolve => {
+    image.onload = () => resolve(STATUS.LOADED)
+    image.onerror = () => resolve(STATUS.ERROR)
+    image.src = url
+  })
+  const timeoutPromise = timeoutPromiseFactory(STATUS.TIMEOUT, image)
+  return Promise.race([imagePromise, timeoutPromise])
+}
 
 const findLocalIp = (): Promise<Array<string>> =>
   new Promise((resolve, reject) => {
@@ -65,7 +69,18 @@ export const findFriends = async (handler: (url: string) => void) => {
     .slice(0, -1)
     .join('.')
   const addresses = Array.from({ length: 256 }, (_, idx) => `http://${baseIp}.${idx}:8000`)
-  addresses.forEach(async address => {
+  // const iterator = async (address: string) => {
+  //   try {
+  //     const status = await ping(address)
+  //     return status ? address : 0
+  //   } catch {
+  //     return 0
+  //   }
+  // }
+  // const results = (await pMap(addresses, iterator)) as (0 | string)[]
+  // const validAddresses = results.filter(r => r !== 0)
+  // console.log('validAddresses', validAddresses)
+  addresses.forEach(async (address: string) => {
     try {
       await window.fetch(address, { mode: 'no-cors' })
       handler(address)
